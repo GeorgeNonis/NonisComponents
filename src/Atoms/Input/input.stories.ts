2dd7c0c587c6f24df698e03ab3c5d26d1ce93ec8
GeorgeNonis <georgenonis@gmail.com>
2023-12-05 00:07:07 +0100

    stories, need to find out tho a standard style

--- src/Atoms/Input/input.stories.ts
+++ src/Atoms/Input/input.stories.ts
@@ -1,4 +1,15 @@
 import { StoryObj, Meta } from "@storybook/react";
 import Input from ".";
 
-export default {} as Meta<typeof Input>;
+export default {
+  component: Input,
+  args: {
+    placeholder: "Placeholder",
+  },
+} as Meta<typeof Input>;
+
+export const Default: StoryObj<typeof Input> = {
+  args: {
+    placeholder: "Default",
+  },
+};
