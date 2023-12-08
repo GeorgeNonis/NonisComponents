19851ebd9aaa34cde949fc2867987a4b7ef38366
GeorgeNonis <georgenonis@gmail.com>
2023-12-08 01:42:42 +0100

    doc: edited documentation on heading

--- src/Atoms/Heading/index.tsx
+++ src/Atoms/Heading/index.tsx
@@ -4,10 +4,12 @@ import { HeadingProps } from "./heading.types";
 
 /**
  * Creates a Heading component that can dynamically render different HTML heading elements (h1, h2, etc.).
- * @param {string} as - The heading element type (e.g., 'h1', 'h2', etc.).
+ * @param {string} as - The heading element type - 'h1' |  'h2' |  'h3' |  'h4' |  'h5' |  'h6'.
+ * @param {TextProps} props - Extented props of the element(<span/>) and stitches/react.
  * @example
- * <Heading as='h1'>This is a heading</Heading>
+ * <Heading as='h1' fontWeight='300'>This is a heading</Heading>
  */
+
 const Heading = forwardRef<HTMLHeadElement, HeadingProps>(
   ({ as, children, ...props }, ref) => {
     return (
