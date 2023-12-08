ebd88b5d88dc5927afa1a3c7f94c9f099562d612
GeorgeNonis <georgenonis@gmail.com>
2023-12-08 01:40:11 +0100

    doc: added comments on heading & text

--- src/Atoms/Heading/index.tsx
+++ src/Atoms/Heading/index.tsx
@@ -2,9 +2,19 @@ import { forwardRef } from "react";
 import { StyledText } from "../Text/text.styles";
 import { HeadingProps } from "./heading.types";
 
+/**
+ * Creates a Heading component that can dynamically render different HTML heading elements (h1, h2, etc.).
+ * @param {string} as - The heading element type (e.g., 'h1', 'h2', etc.).
+ * @example
+ * <Heading as='h1'>This is a heading</Heading>
+ */
 const Heading = forwardRef<HTMLHeadElement, HeadingProps>(
-  ({ as, ...props }, ref) => {
-    return <StyledText ref={ref} {...props} as={as} />;
+  ({ as, children, ...props }, ref) => {
+    return (
+      <StyledText ref={ref} {...props} as={as}>
+        {children}
+      </StyledText>
+    );
   }
 );
 export default Heading;
--- src/Atoms/Text/index.tsx
+++ src/Atoms/Text/index.tsx
@@ -2,6 +2,14 @@ import { forwardRef } from "react";
 import { StyledText } from "./text.styles";
 import { TextProps } from "./text.types";
 
+/**
+ * Text component for rendering text with customizable styles and size.
+ * @param {ReactNode} children - The content of the text component.
+ * @param {string} size - Variant - sets the font-size of the text - default 'm' - 'xs' | 's' | 'm' | 'l' 'xl' .
+ * @param {string} fontWeight - Variant - sets the font-weight.
+ * @param {string} textDecoration - Variant - sets the text-decoration - 'none' | 'underline' | 'line-through'  .
+ * @param {StyledTextProps} props - extented props of the element(<span/>) and stitches/react.
+ */
 const Text = forwardRef<HTMLSpanElement, TextProps>(
   ({ children, size = "m", ...props }, ref) => {
     return (
