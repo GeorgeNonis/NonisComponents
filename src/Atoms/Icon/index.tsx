820584bfeb48ef7322b38c2f3bed3facae22fb68
GeorgeNonis <georgenonis@gmail.com>
2023-12-09 19:29:16 +0100

    chore & doc: added @fontawesome/solid-svg-icons & documentation on the Icon

--- src/Atoms/Icon/index.tsx
+++ src/Atoms/Icon/index.tsx
@@ -1,6 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProps } from "./icon.types";
 
+/**
+ *
+ * @note By default, designed to work with icons from `@fortawesome` either `free`, `solid`, `regular`, `brands`, or `pro`.
+ * @param {IconProp} icon - Svg-Icon to render out
+ * @param {number} height - Optional - sets the height of the Icon default value is 30px
+ * @param {number} width - Optional - sets the width of the Icon default value is 30px
+ * @param {FontAwesomeIconProps} props - Extented props of the FontAwesomeIconProps.
+ *
+ * @example
+ * <Icon icon={faArrowDown} />
+ */
+
 const Icon = ({ style, width = 30, height = 30, ...props }: IconProps) => {
   return <FontAwesomeIcon {...props} style={{ height, width, ...style }} />;
 };
